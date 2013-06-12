c245f353965b6ce0cacc0e59cf3bc038d6a5d3b0
tjmehta <tejesh.mehta@gmail.com>
2013-06-12 12:16:54 -0700

    initial commit, openError method

---
+++ index.js
@@ -0,0 +1,38 @@
+var exec = require('child_process').exec;
+
+function noop() {};
+
+function filepathsInStack(stack) {
+  var fileArray = stack.match(/\(.*\)/mg);
+
+  return fileArray
+    .filter(function () {
+      return file.slice(0, 2) == '(/'; // filter filepaths that do not start with slash (internal node files)
+    })
+    .map(function (file) {
+      return file.slice(1).slice(0, -1); // slice of parenthesis: (filepath) -> filepath
+    })
+  ;
+}
+
+var subl = module.exports = function (args, cb) {
+  if (typeof args == 'string') {
+    args = [args];
+  }
+  else if (!Array.isArray(args)) {
+    throw new Error("'args' must be an array or a string");
+  }
+  cb = cb || noop;
+
+  var sublCmd = 'subl '+args.join(' ');
+
+};
+subl.openError = function (err) {
+  if (err.stack) {
+    var args = ['-n'].concat(filepathsInStack(err.stack));
+    subl(args);
+  }
+  else {
+    throw new Error("'err' has no 'stack'");
+  }
+};
